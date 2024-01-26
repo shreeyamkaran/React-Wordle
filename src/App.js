ee9dd2cdaf1127669ef846d444b35fb5e580bbf7
shreeyamkaran <karanshreeyam@gmail.com>
2024-01-26 12:54:32 +0530

    Set json-server and Generate random word

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
 function App() {
+
+  const [solution, setSolution] = useState("");
+
+  useEffect(function() {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:8000/solutions");
+        const data = await response.json();
+        const index = Math.floor(Math.random() * data.length);
+        setSolution(data[index].word);
+      }
+      catch(error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [setSolution]);
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <h1>{solution}</h1>}
     </div>
   );
 }
